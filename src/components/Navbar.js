862f16f574011abcab045beb9f4300e69db50cb8
Eleni <57090719+eleniaslanidou89@users.noreply.github.com>
2021-08-13 12:42:42 +0200

    added components and pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            TRVL <i class="fab fa-typo3" />
+            Eleni's Blog <i class="fas fa-plane" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
